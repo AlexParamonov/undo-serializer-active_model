b4d534e59db08e7ac5b9e03332b3c9f616b5e424
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-03-21 00:32:37 +0200

    Integration specs

---
+++ spec/integration/undo_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+require "undo"
+require "support/active_record"
+require "user"
+
+Undo.configure do |config|
+  config.serializer = Undo::Serializer::ActiveModel.new
+end
+
+# TODO: extract to shared test
+describe Undo do
+  let(:object) { create :user }
+  subject { described_class }
+
+  it "stores and restores object" do
+    uuid = subject.store object
+    expect(subject.restore uuid).to eq object
+  end
+
+  if Undo::VERSION > "0.1.1"
+    it "deletes stored object" do
+      uuid = subject.store object
+      subject.delete uuid
+      expect { subject.restore uuid }.to raise_error(KeyError)
+    end
+  end
+
+  describe "special cases" do
+    it "stores and restores nil" do
+      uuid = subject.store nil
+      expect(subject.restore uuid).to eq nil
+    end
+
+    it "stores and restores array" do
+      uuid = subject.store [:foo]
+      expect(subject.restore uuid).to eq [:foo]
+    end
+  end
+end
